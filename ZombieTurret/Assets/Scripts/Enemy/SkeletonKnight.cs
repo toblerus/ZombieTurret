9f7c0fb56890af1a051dc0ada3935be83d1e251b
Flavio Engel <flavio.engel@fluffyfairygames.com>
2018-09-15 15:53:45 +0200

    SkeletonKnight

--- ZombieTurret/Assets/Scripts/Enemy/SkeletonKnight.cs
+++ ZombieTurret/Assets/Scripts/Enemy/SkeletonKnight.cs
@@ -7,19 +7,56 @@ namespace Enemy
 {
     public class SkeletonKnight : AbstractEnemy
     {
+        protected override void DistanceToPlayer(int distance)
+        {
+            AttackLogic(distance);
+
+            MovementLogic(distance);
+        }
+
+        public float AttackDistance = 0.5f;
+
+        private void MovementLogic(int distance)
+        {
+            if (distance > AttackDistance)
+            {
+                if (_movementDisposable.IsDisposed)
+                {
+                    Observable.EveryUpdate().Subscribe(x => { Movement(); }).AddTo(_movementDisposable);
+                }
+            }
+            else
+            {
+                _movementDisposable.Dispose();
+                _rigidBody.velocity = Vector2.zero;
+            }
+        }
+
+        private void AttackLogic(int distance)
+        {
+            if (distance < AttackDistance)
+            {
+                Attack();
+            }
+            else
+            {
+                _knightAttackTimer.Dispose();
+            }
+        }
+
+        readonly CompositeDisposable _knightAttackTimer = new CompositeDisposable();
+
         protected override void Attack()
         {
-            Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x =>
-            {
-                DoDamage();
-                //Debug.Log("DOING DAMAGE");
-            }).AddTo(gameObject);
+            if (_knightAttackTimer.Count == 0)
+                Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x => { DoDamage(); })
+                    .AddTo(_knightAttackTimer)
+                    .AddTo(gameObject);
         }
 
         protected override void Movement()
         {
             var rnd = Random.Range(_minMovementSpeed, _maxMovementSpeed);
-
             _rigidBody.velocity = Vector2.left * rnd;
         }
     }
