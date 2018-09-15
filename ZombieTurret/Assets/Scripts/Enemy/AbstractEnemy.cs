9ad992887d1b98380186edbb99b0e295cc657a5b
Flavio Engel <flavio.engel@fluffyfairygames.com>
2018-09-15 11:28:39 +0200

    Created base class for Enemy

---
+++ ZombieTurret/Assets/Scripts/Enemy/AbstractEnemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public abstract class AbstractEnemy : MonoBehaviour
+    {
+        private int _life;
+        private int _movementSpeed;
+
+
+        private  void TakeDamage()
+        {
+
+        }
+
+        private void DoDamage()
+        {
+
+        }
+
+        private void Movement()
+        {
+
+        }
+
+    }
+}
