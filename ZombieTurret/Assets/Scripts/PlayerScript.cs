d7608612e2101af74481740b0063d4e82872543f
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-09-15 12:05:13 +0200

    PlayerControl now working
    - added arrow prefab
    - moving tower

--- ZombieTurret/Assets/Scripts/PlayerScript.cs
+++ ZombieTurret/Assets/Scripts/PlayerScript.cs
@@ -6,31 +6,46 @@ public class PlayerScript : MonoBehaviour {
 
     Vector3 aimPosition;
 
-    GameObject bulletPrefab;
+    public GameObject bulletPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float projectileForce = 500;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        aimPosition = Input.mousePosition;
+        //subtract depth
+        aimPosition.z = transform.position.z - Camera.main.transform.position.z;
+        //normalize
+        aimPosition = Camera.main.ScreenToWorldPoint(aimPosition);
+
+        changeRotation();
+
         if (Input.GetMouseButtonDown(0)) {
-            aimPosition = Input.mousePosition;
-            //subtract depth
-            aimPosition.z = transform.position.z - Camera.main.transform.position.z;
-            //normalize
-            aimPosition = Camera.main.ScreenToWorldPoint(aimPosition);
 
-            shoot();            
+            
+            shoot();
         }
-	}
+
+    }
+
+    void changeRotation() {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
 
     void shoot() {
         var q = Quaternion.FromToRotation(Vector3.up, aimPosition - transform.position);
         var bullet = Instantiate(bulletPrefab, transform.position, q);
         //add specific logic from framework
-        bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.up *  500);
+        bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.up * projectileForce);
 
     }
 }
