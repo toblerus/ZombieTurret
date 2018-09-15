75102a0567e9de612aec25f4365a0fe360969748
toblerus <37512320+toblerus@users.noreply.github.com>
2018-09-15 21:04:52 +0200

    Added and wired the timer

--- ZombieTurret/Assets/Scripts/MenuScript.cs
+++ ZombieTurret/Assets/Scripts/MenuScript.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Assets.Scripts.UI_Scripts;
+using Unity.Linq;
+using System.Linq;
 
 public class MenuScript : MonoBehaviour
 {
@@ -23,5 +26,15 @@ public class MenuScript : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void ContinueGameAfterShop()
+    {
+        FindObjectOfType<PlayerScript>().enabled = true;
+        FindObjectOfType<ShopController>().gameObject.Child("ShopUI").gameObject.SetActive(false);
+        FindObjectOfType<Timer>().Reset();
+        FindObjectOfType<EnemySpawner>().StartSpawning();
+
+
     }
 }
