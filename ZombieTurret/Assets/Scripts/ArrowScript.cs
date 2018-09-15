c9a74c008b4ccc923fc1a9ce8d5cbe74a8fba2f1
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-09-15 12:25:18 +0200

    update

---
+++ ZombieTurret/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float killVelocity = 10;
+
+	// Use this for initialization
+	void Start () {
+		rb  = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rb.velocity.magnitude < killVelocity) {
+            //var c = GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject,1);
+        }
+		
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
