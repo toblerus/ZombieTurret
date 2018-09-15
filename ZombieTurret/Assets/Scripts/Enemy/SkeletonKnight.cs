1f75c4648a12bca9d187902423d42c2934479691
Greg <grzegorz.rus@gmail.com>
2018-09-15 15:20:36 +0200

    Changed script name for basic enemy

---
+++ ZombieTurret/Assets/Scripts/Enemy/SkeletonKnight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Enemy
+{
+    public class SkeletonKnight : AbstractEnemy
+    {
+        protected override void Attack()
+        {
+            Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x =>
+            {
+                DoDamage();
+                //Debug.Log("DOING DAMAGE");
+            }).AddTo(gameObject);
+        }
+
+        protected override void Movement()
+        {
+            var rnd = Random.Range(_minMovementSpeed, _maxMovementSpeed);
+
+            _rigidBody.velocity = Vector2.left * rnd;
+        }
+    }
+}
