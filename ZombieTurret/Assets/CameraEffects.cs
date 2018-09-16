a898cfa1d88e7e77c516a6cb5c6ca5aca41c69ed
Flavio Engel <flavio.engel@fluffyfairygames.com>
2018-09-16 11:00:02 +0200

    Pixelation

--- ZombieTurret/Assets/CameraEffects.cs
+++ ZombieTurret/Assets/CameraEffects.cs
@@ -12,22 +12,22 @@ public class CameraEffects : MonoBehaviour
 
     private float rnd2;
 
-    void Start() { 
-    //{
-    //    Observable.Interval(TimeSpan.FromSeconds(rnd2)).Subscribe(x =>
-    //    {
-    //        var rnd = Random.Range(0.3f, 0.8f);
-
-    //        rnd2 = Random.Range(1, 2f);
-
-    //        EffectBleedingColors.shift = rnd;
-
-
-    //    }).AddTo(gameObject);
-    }
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        {
+            Observable.Interval(TimeSpan.FromSeconds(rnd2)).Subscribe(x =>
+            {
+                var rnd = Random.Range(0.3f, 0.8f);
+
+                rnd2 = Random.Range(1, 2f);
+
+                EffectBleedingColors.shift = rnd;
+
+
+            }).AddTo(gameObject);
+        }
+
+        // Update is called once per frame
+
     }
 }
